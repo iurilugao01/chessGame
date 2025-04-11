7e0e8c1da252440baaa1464b43c67eda602517b3
iurilugao01 <iurilugao01@gmail.com>
2025-04-11 14:25:03 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import TheTable from "./components/TheTable.vue";
+</script>
+
+<template>
+<TheTable></TheTable>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/TheTable.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <table class="table-auto">
+    <tbody>
+      <tr v-for="row in 8" :key="row" class="border-b border-gray-200">
+        <td
+          v-for="col in 8"
+          :class="{
+            'p-3': true,
+            'bg-white': (row + col) % 2,
+            'bg-black': !((row + col) % 2),
+          }"
+        ></td>
+      </tr>
+    </tbody>
+  </table>
+</template>
